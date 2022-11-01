9c345c5771bad018804bd67ab17d2de61ffb52a1
moybadajoz <moisesbadajoz36@gmail.com>
2022-11-01 09:45:29 -0600

    fix

--- wero/settings.py
+++ wero/settings.py
@@ -77,8 +77,8 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'restaurante_db',
-        'USER': 'user1',
-        'PASSWORD': 'aserejejareje2',
+        'USER': '',
+        'PASSWORD': '',
         'HOST': '127.0.0.1',
         'PORT': '3306',
         'OPTIONS':{
